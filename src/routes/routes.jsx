bf5f16d4a95b496bcd6f1f224c8adec395601475
Ndekocode <arickbulakali@gmail.com>
2022-11-24 10:29:13 +0200

    add routes table for more flexibilities

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,8 +1,50 @@
+import { lazy, Suspense } from "react";
 import { Navigate } from "react-router-dom";
+import Loading from "../components/Loading";
+const Login = lazy(() => import("../pages/Login"));
+const Register = lazy(() => import("../pages/Register"));
+const Chat = lazy(() => import("../pages/Chat"));
 const routes = [
   {
     path: "/",
+    protected: false,
     component: <Navigate to="/login" replace />,
+  },
+  {
+    path: "/login",
+    protected: false,
+    component: (
+      <Suspense fallback={<Loading />}>
+        <Login />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/register",
+    protected: false,
+    component: (
+      <Suspense fallback={<Loading />}>
+        <Register />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/chat",
+    protected: true,
+    component: (
+      <Suspense fallback={<Loading />}>
+        <Chat />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/chat/:user",
+    protected: true,
+    component: (
+      <Suspense fallback={<Loading />}>
+        <Chat />
+      </Suspense>
+    ),
   },
 ];
 export default routes;
